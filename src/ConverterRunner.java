b870a43641f439536491056dce836437ee6a7e75
andym <amejia9149@bths.edu>
2023-01-30 22:52:19 -0500

    checked restrictions

--- src/ConverterRunner.java
+++ src/ConverterRunner.java
@@ -9,7 +9,7 @@ public class ConverterRunner {
 
         Scanner s = new Scanner(System.in);
         String choice = s.nextLine();
-        System.out.println("Emter your other base:");
+        System.out.println("Enter your other base:");
         String otherChoice=s.nextLine();
         int otherBase=Integer.parseInt(otherChoice);
 
@@ -18,15 +18,14 @@ public class ConverterRunner {
         String number = s.nextLine();
         int n = Integer.parseInt(number);
         s.close();
-            NumberConverter nc = new NumberConverter(n, base,otherBase);
-            if(nc.restrict(n,base)){
-                int[] digits = nc.getDigits();
-                System.out.println("\n\nDigit array: " + Arrays.toString(digits));
-                System.out.println("Number: " + nc.displayOriginalNumber());
-                System.out.println(nc.displayConvertedNumber());
-            }else{
-                System.out.println("NOT APPLICABLE IN THIS BASE. TRY AGAIN");
-            }
+        NumberConverter nc = new NumberConverter(n, base,otherBase);
+        if(nc.restrict(n,base)){
+            int[] digits = nc.getDigits();
+            System.out.println("\n\nDigit array: " + Arrays.toString(digits));
+            System.out.println("Number: " + nc.displayOriginalNumber());
+            System.out.println(nc.displayConvertedNumber());
+        }else{
+            System.out.println("NOT APPLICABLE IN THIS BASE. TRY AGAIN");
         }
     }
-
+}
--- src/NumberConverter.java
+++ src/NumberConverter.java
@@ -1,10 +1,8 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class NumberConverter {
     int[] digits;
     int number;
-    int convertHexa;
     int otherBase;
     int base;
     public NumberConverter(int number, int base,int otherBase) {
@@ -33,77 +31,33 @@ public class NumberConverter {
         return digits;
     }
     public String convertHexAlphabet(String num) {
-        convertHexa=Integer.parseInt(num);
-        ArrayList<String> unit = new ArrayList<String>();
-        unit.add("0");unit.add("1");
-        unit.add("2");unit.add("3");unit.add("4");
-        unit.add("5");unit.add("6");
-        unit.add("7");unit.add("8");
-        unit.add("9");unit.add("10");
-        unit.add("A");unit.add("B");
-        unit.add("C");unit.add("D");
-        unit.add("E");unit.add("F");
-        unit.add("G");
-        unit.add("H");
-        unit.add("I");
-        unit.add("J");
-        unit.add("K");
-        unit.add("L");
-        unit.add("M");
-        unit.add("N");
-        unit.add("O");
-        unit.add("P");
-        unit.add("Q");
-        unit.add("R");
-        unit.add("S");
-        unit.add("T");
-        unit.add("U");
-        unit.add("V");
-        unit.add("W");
-        unit.add("X");
-        unit.add("Y");
-        unit.add("Z");
-        unit.add("a");
-        unit.add("b");
-        unit.add("c");
-        unit.add("d");
-        unit.add("e");
-        unit.add("f");
-        unit.add("g");
-        unit.add("h");
-        unit.add("i");
-        unit.add("j");
-        unit.add("k");
-        unit.add("l");
-        unit.add("m");
-        unit.add("n");
-        unit.add("o");
-        unit.add("p");
-        unit.add("q");
-        unit.add("r");
-        unit.add("s");
-        unit.add("t");
-        unit.add("u");
-        unit.add("v");
-        unit.add("w");
-        unit.add("x");
-        unit.add("y");
-        unit.add("z");
-        unit.add("+");
-        unit.add("/");
-    for(int i=0;i<unit.size();i++){
-        if(Integer.parseInt(num)<10){
-            return num;
-        }else {
-            if (i >= 10) {
-                if (Integer.parseInt(num) >= base) {
-                    num = num;
-                }
-                num = unit.get(Integer.parseInt(num) + 1);
+        ArrayList<String> unit = new ArrayList<>();
+        unit.add("0");unit.add("1");unit.add("2");unit.add("3");unit.add("4");
+        unit.add("5");unit.add("6");unit.add("7");unit.add("8");unit.add("9");
+        unit.add("10");unit.add("A");unit.add("B");unit.add("C");unit.add("D");
+        unit.add("E");unit.add("F");unit.add("G");unit.add("H");unit.add("I");
+        unit.add("J");unit.add("K");unit.add("L");unit.add("M");unit.add("N");
+        unit.add("O");unit.add("P");unit.add("Q");unit.add("R");unit.add("S");
+        unit.add("T");unit.add("U");unit.add("V");unit.add("W");unit.add("X");
+        unit.add("Y");unit.add("Z");unit.add("a");unit.add("b");unit.add("c");
+        unit.add("d");unit.add("e");unit.add("f");unit.add("g");unit.add("h");
+        unit.add("i");unit.add("j");unit.add("k");unit.add("l");unit.add("m");
+        unit.add("n");unit.add("o");unit.add("p");unit.add("q");unit.add("r");
+        unit.add("s");unit.add("t");unit.add("u");unit.add("v");unit.add("w");
+        unit.add("x");unit.add("y");unit.add("z");unit.add("+");unit.add("/");
+        for(int i=0;i<unit.size();i++){
+            if(Integer.parseInt(num)<10){
                 return num;
-            }
-        }}
-    return num;
+            }else {
+                if (i >= 10) {
+                    if (Integer.parseInt(num) >= base) {
+                        num = num;
+                    }
+                    num = unit.get(Integer.parseInt(num) + 1);
+                    return num;
+                }
+            }}
+        return num;
     }
 
     public int convertToDecimal() {
@@ -125,7 +79,7 @@ public class NumberConverter {
     }
 
     public String convertToBinary() {
-        String binary = "";
+        String binary;
         int remainder;
         StringBuilder binaryOrient= new StringBuilder();
         if (base == 10) {
@@ -188,20 +142,14 @@ public class NumberConverter {
         boolean see=true;
 
         if (base == 8) {
-            String numberAs = Integer.toString(numbers);
-            if (Integer.toString(numbers).contains("8")||Integer.toString(numbers).contains("9")) {
-                this.number = numbers++;
-                return false;
-            }else {
-                return true;
-            }
+            return !Integer.toString(numbers).contains("8") && !Integer.toString(numbers).contains("9");
         }
         if(base==2){
             String[] stringList= new String[Integer.toString(numbers).length()];
             for (int i = 0; i <= Integer.toString(numbers).length() - 1; i++) {
                 stringList[i] = Integer.toString(numbers).substring(i,i+1);
                 if(stringList[i].equals("0")||stringList[i].equals("1")){
-                        see=true;
+                    see=true;
                 }else{
                     see=false;
                     return see;
@@ -211,35 +159,23 @@ public class NumberConverter {
         return see;
     }
     public String convertToHexaDecimal() {
-        String Hexa = "";
-        String compare = "";
         int num;
-        if (base == 10) {
-            num = number;
-        } else {
-            num = convertToDecimal();}
-        while (num / 16 > 0) {
-            Hexa = num % 16 + Hexa;
-            compare = num % 16 + Hexa;
-            Hexa = convertHexAlphabet(Hexa);
-            num /= 16;
+        StringBuilder str= new StringBuilder();
+        if(base==10){
+            num=number;
+        }else{
+            num=convertToDecimal();
         }
-        if (!Hexa.equals(compare)) {
-            Hexa = num % 16 + Hexa;
-            return Hexa;
-        } else {
-            Hexa = num % 16 + Hexa;
-            compare = num % 16 + compare;
-            if(Integer.parseInt(compare)<10) {
-                return compare;
-            }
-            Hexa = convertHexAlphabet(Hexa);
-            return Hexa;
+        while(num>0){
+            String Hex=Integer.toString(num%16);
+            Hex=convertHexAlphabet(Hex);
+            str.insert(0,Hex);
+            num/=16;
         }
+        return str.toString();
     }
     public String toAnyBase(int targetBase){
-        String val=Integer.toString(number);
-        int temp=Integer.parseInt(val);
+        int temp=number;
         String compare="";
         StringBuilder str= new StringBuilder();
         while(temp!=0){
@@ -252,16 +188,14 @@ public class NumberConverter {
     }
     public String displayConvertedNumber() {
         if (base == 8) {
-            return "Binary Number: " + convertToBinary() + "\n" + "Decimal Number: " + convertToDecimal()+"\n"+"Hexadecimal number: "+convertToHexaDecimal();
+            return "Binary Number: " + convertToBinary() + "\n" + "Decimal Number: " + convertToDecimal() + "\n" + "Hexadecimal number: " + convertToHexaDecimal();
         } else if (base == 2) {
-            return "Octal Number: " + convertToOctal() + "\n" + "Decimal Number: " + convertToDecimal()+"\n"+"Hexadecimal number: "+convertToHexaDecimal()+"Desired base: "+otherBase+" "+toAnyBase(otherBase);
+            return "Octal Number: " + convertToOctal() + "\n" + "Decimal Number: " + convertToDecimal() + "\n" + "Hexadecimal number: " + convertToHexaDecimal() + "Desired base: " + otherBase + " " + toAnyBase(otherBase);
         } else if (base == 10) {
-            return "Octal Number: " + convertToOctal() + "\n" + "Binary Number: " + convertToBinary()+"\n"+ "Hexadecimal number: "+convertToHexaDecimal()+"\n"+"Base "+otherBase+" Number: "+toAnyBase(otherBase);
-        } else if (base==16) {
-            return "Bullshit cause I didn't do it yet"+"\n"+"Binary Number: "+convertToBinary();
-        } else {
-            return "Base: "+base+" "+toAnyBase(base);
+            return "Octal Number: " + convertToOctal() + "\n" + "Binary Number: " + convertToBinary() + "\n" + "Hexadecimal number: " + convertToHexaDecimal() + "\n" + "Base " + otherBase + " Number: " + toAnyBase(otherBase);
+        } else if (base == 16) {
+            return "\n" + "Binary Number: " + convertToBinary();
         }
+        return "Done";
     }
-
 }
